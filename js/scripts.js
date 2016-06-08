91b9657497945a0f2cf0cfc3fa52f0c4b22a7743
Simon Temple <Simon Temple>
2016-06-07 22:21:53 -0700

    complete groceries intro project

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+  var resultArray = [];
+  $(".result").hide();
+  $(".form").submit(function(event){
+    event.preventDefault();
+    var userInput = $("#input").val(); //saves form value
+    var inputCapitalized = userInput.toUpperCase(); //capitalizes string
+    resultArray = inputCapitalized.split(" "); //splits all words into array
+    var sortedArray = resultArray.sort(); //sorts array alphabetically
+
+    // an alternative shortened version to the stuff above
+    // var sortedArray = $("#input").val().toUpperCase().split(" ").sort();
+
+    $(".result").show(); //shows result div
+    for (var i = 0; i < sortedArray.length; i++) {
+      $(".groceries").append("<li> " + sortedArray[i] + "</li>"); //appends each item in array to page. ~take extra time to study and fully understand the for loop. this will really help you later.
+    }
+  });
+});
